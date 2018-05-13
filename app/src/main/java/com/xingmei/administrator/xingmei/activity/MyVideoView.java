a655bca8570a5468bef3195e37aa71be34860648
112634077 <1126340778@qq.com>
2018-05-13 16:58:25 +0800

    第二次提交

--- app/src/main/java/com/xingmei/administrator/xingmei/activity/MyVideoView.java
+++ app/src/main/java/com/xingmei/administrator/xingmei/activity/MyVideoView.java
@@ -4,9 +4,13 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 
+import com.tencent.smtt.sdk.TbsVideo;
+
 public class MyVideoView extends Activity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
     }
+
 }
---
+++ app/src/main/java/com/xingmei/administrator/xingmei/application/APPAplication.java
@@ -0,0 +1,28 @@
+package com.xingmei.administrator.xingmei.application;
+
+import android.app.Application;
+import android.util.Log;
+
+import com.tencent.smtt.sdk.QbSdk;
+
+public class APPAplication extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        QbSdk.PreInitCallback cb = new QbSdk.PreInitCallback() {
+            @Override
+            public void onCoreInitFinished() {
+
+            }
+
+            @Override
+            public void onViewInitFinished(boolean b) {
+                //x5內核初始化完成的回调，为true表示x5内核加载成功，否则表示x5内核加载失败，会自动切换到系统内核。
+                Log.d("app", " onViewInitFinished is " + b);
+            }
+        };
+        QbSdk.initX5Environment(getApplicationContext(), cb);
+    }
+}
