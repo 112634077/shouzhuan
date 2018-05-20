7856f09c1afa7eda036bb046ff4791e76367ed39
112634077 <1126340778@qq.com>
2018-05-20 22:20:50 +0800

    第7次提交

--- app/src/main/java/com/xingmei/administrator/xingmei/onclick/ListClick.java
+++ app/src/main/java/com/xingmei/administrator/xingmei/onclick/ListClick.java
@@ -2,9 +2,11 @@ package com.xingmei.administrator.xingmei.onclick;
 
 import android.content.Context;
 import android.content.Intent;
+import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
 
+import com.tencent.smtt.sdk.TbsVideo;
 import com.xingmei.administrator.xingmei.activity.MyWebViewActivity;
 
 public class ListClick implements AdapterView.OnItemClickListener {
@@ -16,8 +18,25 @@ public class ListClick implements AdapterView.OnItemClickListener {
     }
     @Override
     public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-        Intent intent = new Intent(context,MyWebViewActivity.class);
-        intent.putExtra("url",url);
-        context.startActivity(intent);
+//        Intent intent = new Intent(context,MyWebViewActivity.class);
+//        intent.putExtra("url",url);
+//        context.startActivity(intent);
+        startVideo();
+    }
+
+    protected void startVideo(){
+        if (TbsVideo.canUseYunbo(context)){
+            Bundle data = new Bundle();
+
+            data.putBoolean("standardFullScreen", false);
+           //true表示标准全屏，false表示X5全屏；不设置默认false，
+
+            data.putBoolean("supportLiteWnd", false);
+            //false：关闭小窗；true：开启小窗；不设置默认true，
+
+            data.putInt("DefaultVideoScreen", 2);
+            //1：以页面内开始播放，2：以全屏开始播放；不设置默认：1
+            TbsVideo.openYunboVideo(context,"http://aass-10009076.cossh.myqcloud.com/video.mp4",data,null);
+        }
     }
 }
